d90b9f8eadf85accfe36431572c4a8b9ef658f03
Shahbaz Nazir <shahbaznazir1992@gmail.com>
2017-04-17 07:38:06 +0000

    Minor repo restructing
    Signed-off-by: Shahbaz Nazir <shahbaznazir1992@gmail.com>

--- run_tests.sh
+++ run_tests.sh
@@ -4,14 +4,15 @@ set -e
 
 if [ "$TEST_TYPE" = "integ" ]
 then
-  if curl http://localhost/calculate?x=1 | grep -q 'undefined'; then
+  if curl http://server/calculate?x=1 | grep -q 'undefined'; then
     echo "Tests passed!"
     exit 0
   else
     echo "Tests failed!"
     exit 1
   fi
-else
+elif [ "$TEST_TYPE" = "unit" ]
+then
   python setup.py install
   python -m unittest discover -s tgc
 fi
