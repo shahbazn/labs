f353d171b09c06fe1a3d204708149647da743b7d
Shahbaz Nazir <shahbaznazir1992@gmail.com>
2017-04-19 06:41:45 +0000

    Added C/I pipeline automation
    Signed-off-by: Shahbaz Nazir <shahbaznazir1992@gmail.com>

--- run_tests.sh
+++ run_tests.sh
@@ -4,15 +4,14 @@ set -e
 
 if [ "$TEST_TYPE" = "integ" ]
 then
-  if curl http://web/calculate?x=1 | grep -q 'undefined'; then
+  if curl http://"$DOMAIN"/calculate?x=1 | grep -q 'undefined'; then
     echo "Tests passed!"
     exit 0
   else
     echo "Tests failed!"
     exit 1
   fi
-elif [ "$TEST_TYPE" = "unit" ]
-then
+else
   python setup.py install
   python -m unittest discover -s tgc
 fi
